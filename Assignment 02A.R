bc2e3fbc2329bd4ff94e3ab5e66f66237e0eac97
Carolyn Taylor <carolyntaylor11224@gmail.com>
2022-02-27 13:58:45 -0500

    assignment 2 grade

--- Assignment 02A.R
+++ Assignment 02A.R
@@ -1,3 +1,3 @@
 AgeDays = as.double(as.Date("2022-05-06") - Sys.Date())
 cat("Vietri, there are",AgeDays,"days left in the semester")
-
+#50/50
--- Assignment 02B.R
+++ Assignment 02B.R
@@ -5,4 +5,4 @@ theDF <- data.frame(x,y,z)
 names(theDF)<-c("Popularity", "Team Strength", "Sport")
 
 sd(theDF$`Team Strength`)
-
+#50/50
